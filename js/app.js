ec94000f5729aa72b77be5cc6d91ea58f943eb2b
Gloper98 <littleannygutlop_2014@outlook.es>
2017-12-14 21:22:04 -0500

    Updating changes bootstrap maquetado

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
 // La funcionalidad de tu proyecto
+window.addEventListener('load', function(event) {
+    
+    var buttonDropdown = document.getElementById('button-dropdown');
+    var content = document.getElementsByTagName('main')[0];
+    
+    buttonDropdown.addEventListener('click', function(event) {
+        if (content.classList.contains('increse-height')) {
+          content.classList.add('decrease-height');
+          content.classList.remove('increse-height');
+        } else {
+            content.classList.add('increse-height');
+            content.classList.remove('decrease-height');
+        }
+    })
+})
